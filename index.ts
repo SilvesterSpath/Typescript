501b1cb87be67fa0c01b9472defc841d5b16bbb5
SilvesterSpath <spath.sz@gmail.com>
2023-04-17 06:09:42 +0200

    01.09

--- index.ts
+++ index.ts
@@ -1,18 +1,24 @@
 import axios from 'axios';
 
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 const fetchData = async () => {
   try {
     const response = await axios.get(
       'http://jsonplaceholder.typicode.com/todos/1'
     );
 
-    const todo = response.data;
-    const { ID, Title, finished } = todo;
+    const todo = response.data as Todo;
+    const { id, title, completed } = todo;
 
     return `
-      The Todo with ID: ${ID}
-      Has a title of: ${Title}
-      Is if finished? ${finished}
+      The Todo with ID: ${id}
+      Has a title of: ${title}
+      Is if finished? ${completed}
     `;
   } catch (error) {
     console.log(error);
