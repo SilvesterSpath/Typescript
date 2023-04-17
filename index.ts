087f21a32db4bdd965ebafbe0d16e65d20249c8e
SilvesterSpath <spath.sz@gmail.com>
2023-04-17 05:50:16 +0200

    catching errors with typescript

--- index.ts
+++ index.ts
@@ -6,7 +6,14 @@ const fetchData = async () => {
       'http://jsonplaceholder.typicode.com/todos/1'
     );
 
-    return response.data;
+    const todo = response.data;
+    const { ID, Title, finished } = todo;
+
+    return `
+      The Todo with ID: ${ID}
+      Has a title of: ${Title}
+      Is if finished? ${finished}
+    `;
   } catch (error) {
     console.log(error);
     throw error;
